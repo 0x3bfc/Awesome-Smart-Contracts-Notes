17997c4c0d22721bc600f2b20954a2741e55f13e
Ahmed Abdullah <ahmedaabdulwahed@gmail.com>
2019-01-18 17:57:04 +0100

    add internal vs private example

--- examples/03AnonymousEvents/contracts/PlayWithAnonymousEvents.sol
+++ examples/03AnonymousEvents/contracts/PlayWithAnonymousEvents.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.25;
 
 contract PlayWithAnonymousEvents{
     uint256 private counter = 0;
-    event newEvent(string _value, uint256 _counter);
+    event newEvent(string _value, uint256 _counter) anonymous;
     function testEvent() public {
         counter +=1;
         emit newEvent("test event", counter);
--- examples/06InternalvsPrivate/contracts/InternalvsPrivate.sol
+++ examples/06InternalvsPrivate/contracts/InternalvsPrivate.sol
@@ -1,2 +1,26 @@
-pragma solidity ^0.4.25;
+pragma solidity ^0.5.02;
 
+contract InternalVsPrivate{
+    bool private b = false;
+    function f1() internal view returns(bool){
+        return b;
+    }
+    
+    function f2() private view returns(bool){
+        return f1();
+    }
+    
+    function f3() internal view returns(bool){
+        return f2();
+    }
+}
+
+contract Caller is InternalVsPrivate{
+    function testPrivate() public view returns(bool){
+        return f2(); // won't work!
+    }
+    
+    function testInternal() public view returns(bool){
+        return f1(); // works!
+    }
+}

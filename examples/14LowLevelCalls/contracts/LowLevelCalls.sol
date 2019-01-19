922d7a29f5dc9046bd80bf01e2b84174bd90107b
Ahmed Abdullah <ahmedaabdulwahed@gmail.com>
2019-01-19 15:19:51 +0200

    update low level calls

--- examples/14LowLevelCalls/contracts/LowLevelCalls.sol
+++ examples/14LowLevelCalls/contracts/LowLevelCalls.sol
@@ -1,6 +1,6 @@
-pragma solidity ^0.5.2;
+pragma solidity ^0.4.25;
 
-contract CallCodeCallee{
+contract LowLevelCallsExample{
     mapping(address => bytes32[]) assets;
     function register(bytes32 asset) public returns(bool){
         assets[msg.sender].push(asset);
@@ -8,6 +8,7 @@ contract CallCodeCallee{
 }
 
 contract CallCodeCaller{
+    // call LowLevelCallsExample.register
     address callee;
     
     constructor(address _callee) public {
@@ -20,7 +21,15 @@ contract CallCodeCaller{
 }
 
 
-contract CallCaller{
+contract Callee{
+    uint256 number;
+    
+    function register(uint256 _number)public returns(bool){
+        number = _number;
+    }
+}
+
+contract CallCaller1{
     address registryContract;
     constructor(address _registeryContract) public{
         registryContract = _registeryContract;
@@ -31,10 +40,25 @@ contract CallCaller{
     }
 }
 
-contract CallCallee{
-    uint256 number;
+contract CallCaller2{
+    address registryContract;
+    constructor(address _registeryContract) public{
+        registryContract = _registeryContract;
+    }
     
-    function register(uint256 _number)public returns(bool){
-        number = _number;
+    function register(uint256 number) public returns(bool){
+        return registryContract.call(bytes4(keccak256('register()')), number);
+    }
+}
+
+
+contract DelegateCalls{
+    function delegateMyCall(
+        address _callee, 
+        uint256 _number
+    )
+    public
+    returns (bool){
+        return _callee.delegatecall(bytes4(keccak256('register')), _number);
     }
 }

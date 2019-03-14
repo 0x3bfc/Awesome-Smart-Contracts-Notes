b491cb40fc50ddc0855cd904d466abd38f8ea825
Ahmed Abdullah <ahmedaabdulwahed@gmail.com>
2019-03-14 17:40:19 +0100

    add second manticore example

--- examples/manticore-examples/2.ManipulateSmartContract/README.md
+++ examples/manticore-examples/2.ManipulateSmartContract/README.md
@@ -7,5 +7,9 @@ For installation, please check the [Preliminaries section in first example](../1
 
 ## Run
 ```bash
-
+docker run -t -v $(pwd):/src aabdulwahed/manticore:solc-0.5.3 python3 /src/manipulate_smart_contract.py 
+[+] Blockchain initialized 
+[+] Created user account  user_account
+[+] Initiate the contract  1003150779044222824843138452687820282225015987163
+[+] Look for results in  /mcore_1bi0uswg
 ```
--- examples/manticore-examples/2.ManipulateSmartContract/manipulate_smart_contract.py
+++ examples/manticore-examples/2.ManipulateSmartContract/manipulate_smart_contract.py
@@ -1,3 +1,57 @@
-import os
-import sys
 from manticore.ethereum import ManticoreEVM
+
+# initiate Blockchain
+m = ManticoreEVM()
+print(f'[+] Blockchain initialized ')
+smart_contract_code = '''
+pragma solidity 0.5.3;
+
+contract SimpleExample{
+    function foo(uint256 a) payable public {
+        if(a == 40){
+            revert();
+        }
+    }
+}
+'''
+
+# create new user account
+user_account = m.create_account(balance=1000, name='user_account')
+print(f'[+] Created user account ', user_account.name_)
+
+# initiate contract
+contract_account = m.solidity_create_contract(
+    smart_contract_code,
+    owner = user_account
+)
+print(f'[+] Initiate the contract ', contract_account)
+
+# create symbolic data and value
+# symbolic data means that the manticore will explore all the
+# contract functions during transaction execution
+# For more information about the function selection check out this link
+# https://blog.trailofbits.com/2017/11/06/hands-on-the-ethernaut-ctf/
+
+symbolic_data = m.make_symbolic_buffer(320) # creates symbolic byte array with size = 320
+symbolic_value = m.make_symbolic_value()
+
+# execution raw transaction using m.transaction
+m.transaction(
+    caller = user_account,
+    address = contract_account,
+    data = symbolic_data,
+    value = symbolic_value
+)
+
+# execute only one function
+# create new symbolic value and call foo()
+# if value in the transaction was not specified, the default is 0
+symbolic_var = m.make_symbolic_value()
+contract_account.foo(
+    symbolic_var,
+    caller = user_account,
+    value = 0
+)
+
+m.finalize()
+print(f"[+] Look for results in ", m.workspace)
